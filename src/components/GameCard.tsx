3cf85e00c2b7c442cb4e7bd887ec86279d2dabeb
Simon Kallkvist <simon.kallkvist@gmail.com>
2024-06-01 11:33:38 +0200

    display critic Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
 
 interface Props {
   game: Game;
@@ -13,6 +15,12 @@ const GameCard = ({ game }: Props) => {
         <Image src={game.background_image} />
         <CardBody>
           <Heading fontSize='2xl'>{game.name}</Heading>
+          <HStack justifyContent={'space-between'}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
         </CardBody>
       </Card>
     </>
